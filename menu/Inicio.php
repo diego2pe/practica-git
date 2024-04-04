b0112d8a31ccba862ef95d5d7503fdacffe358d8
diego2pe <2022016505@unfv.edu.pe>
2024-04-04 18:02:59 -0500

    primer avance de navbar

---
+++ menu/Inicio.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="menu"></nav>  
+        <ul>
+            <li  style="float:left"><a href="#">SC FIBRA</a></li>
+            <li><a href="#">SOBRE NOSOTROS</a></li>
+            <li><a href="#">PRODUCTOS</a></li>
+            <li><a href="#">SERVICIOS</a></li>
+            <li><a href="#">CONTACTANOS</a></li>
+        </ul>
+    
+</body>
+</html>
